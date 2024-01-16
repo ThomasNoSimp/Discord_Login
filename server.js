eadda8643896712810559690594d8dd5b8ca828e
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-01-16 14:50:56 +0630

    !

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
+const fs = require('fs').promises;
 const DiscordOAuth2 = require('discord-oauth2');
 require('dotenv').config();
 
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const discordOAuth = new DiscordOAuth2({
-    clientId: 'YOUR_DISCORD_CLIENT_ID',
+    clientId: '1126708742629634088',
     clientSecret: process.env.DiscordClientSecret,
     redirectUri: 'https://disc-login.netlify.app',
 });
@@ -41,8 +41,8 @@ app.get('/auth/success', async (req, res) => {
         // Retrieve user information using the access token
         const user = await discordOAuth.getUser(tokenData.access_token);
 
-        // Save user credentials to credentials.json
-        saveCredentials(user);
+        // Save user credentials to user-info.json
+        await saveCredentials(user);
 
         // Redirect the user to the custom website
         res.redirect('https://disc-login.netlify.app');
@@ -53,17 +53,20 @@ app.get('/auth/success', async (req, res) => {
 });
 
 // Function to save user credentials to user-info.json
-function saveCredentials(user) {
+async function saveCredentials(user) {
     const userInfoPath = 'user-info.json';
 
     try {
+        // Read existing data from user-info.json or initialize an empty array
         let existingData = [];
-
-        // Check if user-info.json file exists
-        if (fs.existsSync(userInfoPath)) {
-            // Read existing data from user-info.json
-            const data = fs.readFileSync(userInfoPath, 'utf8');
+        try {
+            const data = await fs.readFile(userInfoPath, 'utf8');
             existingData = JSON.parse(data);
+        } catch (readError) {
+            // Ignore read error if the file doesn't exist
+            if (readError.code !== 'ENOENT') {
+                console.error('Error reading user-info.json:', readError);
+            }
         }
 
         // Append new user data to existing data
@@ -76,13 +79,12 @@ function saveCredentials(user) {
         });
 
         // Write updated data back to user-info.json
-        fs.writeFileSync(userInfoPath, JSON.stringify(existingData, null, 2));
+        await fs.writeFile(userInfoPath, JSON.stringify(existingData, null, 2));
         console.log('User information saved successfully to user-info.json:', user);
     } catch (error) {
         console.error('Error handling user credentials:', error);
     }
 }
-
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
