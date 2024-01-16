d14cd65236d9c1cb33922872babfb17c7bd36055
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-01-16 18:57:09 +0630

    !

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post('/saveFormData', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
