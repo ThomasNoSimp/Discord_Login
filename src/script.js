3364211698bd609b4f5bd113a92ac0d60597fc81
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-01-16 18:44:44 +0630

    !

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         // Send the form data to the server
-        fetch('http://localhost:5000/saveFormData', {
+        fetch('http://127.0.0.1:5000/saveFormData', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
